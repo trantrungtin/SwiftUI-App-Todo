bb77d21cc98f33d3cfe22f6aeabee85a0b389a85
Tin Tran <tinhardware@gmail.com>
2022-06-19 14:39:09 +0700

    Update UI

--- Todo App/View/SettingsView.swift
+++ Todo App/View/SettingsView.swift
@@ -32,7 +32,23 @@ struct SettingsView: View {
                 
                 Form {
                     Section(header: Text("Choose the app icon")) {
-                        Picker(selection: $iconSettings.currentIndex, label: Text("App Icons")) {
+                        Picker(selection: $iconSettings.currentIndex, label: (
+                            HStack {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                                        .strokeBorder(.primary, lineWidth: 2)
+                                    
+                                    Image(systemName: "paintbrush")
+                                        .font(.system(size: 28, weight: .regular, design: .default))
+                                        .foregroundColor(.primary)
+                                }
+                                .frame(width: 44, height: 44)
+                                
+                                Text("App Icons".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.primary)
+                            }
+                        )) {
                             ForEach(0..<iconSettings.iconNames.count) {idx in
                                 HStack {
                                     Image(uiImage: UIImage(named: iconSettings.iconNames[idx] ?? "Blue") ?? UIImage())
